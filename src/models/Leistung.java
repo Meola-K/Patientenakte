ebb6b33c293539406f957d2a45db4bf6f342c28f
Med <med.koeker@gmail.com>
2025-05-26 14:49:56 +0200

    Leistungklasse in models eingefügt

---
+++ src/models/Leistung.java
@@ -0,0 +1,30 @@
+package models;
+
+public class Leistung {
+    private String lkNr;
+    private String bezeichnung;
+    private String beschreibung;
+
+    public Leistung (){}
+
+    public String getLkNr() {
+        return lkNr;
+    }
+    public void setLkNr(String lkNr){
+        this.lkNr =lkNr;
+    }
+
+    public String getBezeichnung(String bezeichnung){
+        return bezeichnung;
+    }
+    public void setBezeichnung(String bezeichnung) {
+        this.bezeichnung = bezeichnung;
+    }
+
+    public String getBeschreibung() {
+        return beschreibung;
+    }
+    public void setBeschreibung(String beschreibung) {
+        this.beschreibung = beschreibung;
+    }
+}
