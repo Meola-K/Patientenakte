2edd7adb845dbbbcecd4ddfb694a7b6130a32677
LuckCrafter <jeremy@gottlieb.cyou>
2025-06-23 14:02:53 +0200

    init dataLayer.services Package

--- src/dataLayer/services/DataLayerFactory.java
+++ src/dataLayer/services/DataLayerFactory.java
@@ -1,4 +1,29 @@
 package dataLayer.services;
 
+import configuration.models.*;
+import dataLayer.dataAccessObjects.IDao;
+
 public class DataLayerFactory {
+    private Configuration config;
+    public IDataLayer createDataLayer(Configuration config) {
+        return null;
+    }
+    private IDao createDao(ModelType modelType) {
+        return null;
+    }
+    private IDao createDbDao(ModelType modelType, DataSource dataSource) {
+        return null;
+    }
+    private IDao createFileDao(ModelType modelType, DataSource dataSource) {
+        return null;
+    }
+    private DataSource getDataSource(ModelType modelType) {
+        return null;
+    }
+    private DbConnection getDbConnection(String dbConnection) {
+        return null;
+    }
+    private FileConnection getFileConnection(ModelType modelType) {
+        return null;
+    }
 }
--- src/dataLayer/services/DataLayerManager.java
+++ src/dataLayer/services/DataLayerManager.java
@@ -1,4 +1,16 @@
 package dataLayer.services;
 
 public class DataLayerManager {
+    static DataLayerManager instance;
+    private IDataLayer dataLayer;
+    private DataLayerManager() {}
+    public static DataLayerManager getInstance() {
+        if (instance == null) {
+            instance = new DataLayerManager();
+        }
+        return instance;
+    }
+    public IDataLayer getDataLayer() {
+        return dataLayer;
+    }
 }
