7c3506efdecb1d7d78c31dd9decf65d9c0127ba7
Med <med.koeker@gmail.com>
2025-06-23 13:00:52 +0200

    Leistungklasse in models eingefügt

---
+++ src/dataLayer/dataAccessObjects/IDao.java
@@ -0,0 +1,12 @@
+package dataLayer.dataAccessObjects;
+
+import java.util.List;
+
+public interface IDao <T, ID> {
+    public T create();
+    public T create(T objectToInsert);
+    public T read(ID id);
+    public List<T> read();
+    public void update(T objectToUpdate);
+    public void delete(ID id);
+}
